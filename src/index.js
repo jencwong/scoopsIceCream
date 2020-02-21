3c9b55ad4c9b8b33b273016f65764fe48663e696
Fizal Sarif <fizal.sarif@gmail.com>
2020-02-21 00:42:08 -0500

    add axios example

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 import sum from './sum';
+import axios from 'axios';
+
 
 console.log(sum(1,2));
-console.log(sum(1,2));
+
+const main = async () => {
+  const res = await axios.get('https://fizal.me/pokeapi/api/v2/name/bulbasaur.json');
+  console.log(res.data);
+}
+
+main();
+
+
