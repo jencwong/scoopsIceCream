e3f9ff06a7434d57584f6003587f0cbf0b604c78
Jenwng <Jenniescwong@gmail.com>
2020-02-28 21:31:19 -0500

    displayed ice cream and locations

--- app.js
+++ app.js
@@ -4,6 +4,18 @@ const displayModal = () => {
   $("#myModal").css("display", "flex");
 };
 
+const closeModal = () => {
+  $(".close").on("click", event => {
+    $("#chat-message").text("");
+    $("#myModal").css("display", "none");
+    $("#input-box1").val("");
+  });
+};
+
+// const resetInput = () => {
+//   $("#chat-message").text("");
+// };
+
 let arrKid = [
   "Turns out I'm a pretty damn good babysitter",
   "Man, kids are the worst! Who needs 'em, anyway?",
@@ -19,6 +31,7 @@ const endpoint = "https://project.wnyc.org/ice-cream/data/places.json";
 
 let zipCode = "";
 let ans = [];
+let html = "";
 
 const callAPI = async () => {
   try {
@@ -32,9 +45,21 @@ const callAPI = async () => {
       return ans;
     };
     console.log(filterZip());
+    displayApi();
   } catch (err) {
     console.log(err);
   }
+};
+
+const displayApi = () => {
+  html = "<ul>";
+  // callAPI();
+  console.log(ans);
+  for (let i = 0; i < ans.length; i++) {
+    // html += "<li>something</li>";
+    html += `<li>${ans[i].name} address: ${ans[i].address}</li>`;
+  }
+  html += "</ul>";
 };
 
 $(() => {
@@ -93,30 +118,36 @@ $(() => {
     if (question.includes("scoops ahoy")) {
       //   $("#myModal").css("display", "flex");
       displayModal();
-
       $("#chat-message").text("Scoops Ahoy!");
+      closeModal();
+      resetInput();
     } else if (question.includes("where " || "located" || "found" || "find")) {
       //   $("#myModal").css("display", "flex");
       displayModal();
 
       $("#chat-message").text("Starcourt mall, Hawkins Indiana");
+      closeModal();
     } else if (question.includes("kid")) {
       displayModal();
       $("#chat-message").text(arrKid[Math.floor(Math.random() * 3) + 1]);
+      closeModal();
     } else if (question.includes("zip")) {
       zipCode = question.slice(-6, -1);
       console.log(typeof zipCode);
+      // let html = "<ul>";
+      // callAPI();
       callAPI();
-      displayModal();
-      let html = "<ul>";
-      for (let i = 0; i < ans.length; i++) {
-        html += `<li>${ans[i].name}</li>`;
-      }
-      html += "</ul>";
+      // for (let i = 0; i < ans.length; i++) {
+      //   html += `<li>${ans[i].name}</li>`;
+      // }
+      // html += "</ul>";
       $("#chat-message").append(html);
+      setTimeout(displayModal(), 3000);
+      closeModal();
     } else {
       displayModal();
       $("#chat-message").text("Yeah, that's a no.");
+      closeModal();
     }
   });
 });
